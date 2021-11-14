159a059c25abc56365bfd23b87465e8f3abd0c2d
Alex <bardovski.alesha@mail.ru>
2021-11-14 17:28:13 +0300

    added auto-documenting from code

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
 import com.example.consumingwebservice.wsdl.*;
+import com.example.demo.exeptions.ErrorResponse;
 import com.example.demo.exeptions.SoapClientException;
+import com.example.demo.exeptions.ViolationOfVerificationResponse;
 import com.example.demo.soapclient.CalculatorClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,8 +18,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 
+@Tag(
+        name="Calculator controller",
+        description="REST адаптер к SOAP сервису http://www.dneonline.com/calculator.asmx."
+)
 @Validated
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "500",content = @Content(schema = @Schema(
+                implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "400",content = @Content(schema = @Schema(
+                implementation = ViolationOfVerificationResponse.class)))
+
+})
 public class CalculatorController {
 
     private final CalculatorClient soapClient;
@@ -20,7 +39,15 @@ public class CalculatorController {
         this.soapClient = soapClient;
     }
 
+    @Operation(
+            summary = "Сложение",
+            description = "Складывает числа A и B"
+    )
     @RequestMapping(path = "/add", method = RequestMethod.GET)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",content = @Content(schema = @Schema(
+                    implementation = AddResponse.class)))
+    })
     public ResponseEntity<?> getAdd(@RequestParam @NotNull int A, @RequestParam @NotNull int B)
             throws SoapClientException{
         Add request = new Add();
@@ -38,6 +65,14 @@ public class CalculatorController {
 
     }
 
+    @Operation(
+            summary = "Деление",
+            description = "Делит число A на число B"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",content = @Content(schema = @Schema(
+                    implementation = DivideResponse.class)))
+    })
     @RequestMapping(path = "/divide", method = RequestMethod.GET)
     public ResponseEntity<?> getDivide(@RequestParam @NotNull int A, @RequestParam @NotNull int B)
             throws SoapClientException,ArithmeticException {
@@ -60,6 +95,15 @@ public class CalculatorController {
         return new ResponseEntity<>(response,HttpStatus.OK);
 
     }
+
+    @Operation(
+            summary = "Умножение",
+            description = "Умножает числа A и B"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",content = @Content(schema = @Schema(
+                    implementation = MultiplyResponse.class)))
+    })
     @RequestMapping(path = "/multiply", method = RequestMethod.GET)
     public ResponseEntity<?> getMultiply(@RequestParam @NotNull int A, @RequestParam @NotNull int B)throws SoapClientException{
         Multiply request = new Multiply();
@@ -76,6 +120,15 @@ public class CalculatorController {
         return new ResponseEntity<>(response,HttpStatus.OK);
 
     }
+
+    @Operation(
+            summary = "Вычитание",
+            description = "Вычитает число B из числа A"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",content = @Content(schema = @Schema(
+                    implementation = SubtractResponse.class)))
+    })
     @RequestMapping(path = "/subtract", method = RequestMethod.GET)
     public ResponseEntity<?> getSubtract(@RequestParam @NotNull int A, @RequestParam @NotNull int B)throws SoapClientException{
         Subtract request = new Subtract();
